9479e5312a29e8126780ee159a952194b572bb50
YounGoat <youngoat@163.com>
2016-06-28 01:18:21 +0800

    v0.1.9 redone

--- template/entry.bottom.js
+++ template/entry.bottom.js
@@ -1,4 +1,5 @@
-(function() {
+;(function() {
+    // ...
 	var global = (function() { return this; })();
 	global.requireJs(['MOLES_ENTRY_MODULE_NAME'], function(entry) {
 		global.re2('MOLES_ENTRY_MODULE_NAME');
